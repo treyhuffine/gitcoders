121300e33ff896929fb548960ded4f641ae9585e
Trey Huffine <treyhuffine@gmail.com>
2015-08-03 10:43:55 -0700

    Update navbar styling

--- public/javascripts/components/navbar/NavGC.jsx
+++ public/javascripts/components/navbar/NavGC.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import { Link } from 'react-router';
 
 let logoStyle = {
-  'marginLeft': '10px'
+  'marginLeft': '10px',
+  'color': '#FCFCFD'
+}
+let navButtons ={
+  'color': '#d5d5d7'
 }
 
 export default class NavGC extends React.Component {
@@ -23,12 +27,12 @@ export default class NavGC extends React.Component {
       let username = this.props.currentUser.githubData.login;
       return (
         <nav>
-          <div className="nav-wrapper black z-depth-1">
+          <div className="nav-wrapper grey darken-4 z-depth-1">
             <a href="#" className="brand-logo" style={logoStyle}>gitcoders</a>
             <ul className="right hide-on-med-and-down">
-              <li className="navButtons"><Link to={`/git/${username}`}>View Profile</Link></li>
-              <li className="navButtons"><Link to={`/git/${username}`}>Edit Profile</Link></li>
-              <li className="navButtons"><a href="/auth/logout">Logout</a></li>
+              <li className="navButtons"><Link to={`/git/${username}`} style={navButtons}>View Profile</Link></li>
+              <li className="navButtons"><Link to={`/git/${username}`} style={navButtons}>Edit Profile</Link></li>
+              <li className="navButtons"><a href="/auth/logout" style={navButtons}>Logout</a></li>
             </ul>
             <ul className="side-nav" id="mobile-demo">
               <li className="navButtons"><a href="/auth/logout">Logout</a></li>
@@ -39,10 +43,10 @@ export default class NavGC extends React.Component {
     } else {
       return (
         <nav>
-          <div className="nav-wrapper black z-depth-1">
+          <div className="nav-wrapper grey darken-4 z-depth-1">
             <a href="#" className="brand-logo" style={logoStyle}>gitcoders</a>
             <ul className="right hide-on-med-and-down">
-              <li className="navButtons"><a href="/auth/github">Login</a></li>
+              <li className="navButtons"><a href="/auth/github" style={navButtons}>Login</a></li>
             </ul>
             <ul className="side-nav" id="mobile-demo">
               <li className="navButtons"><a href="/auth/github">Login</a></li>
