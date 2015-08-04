96ecb3169a41099521e6345dc5edab2ec50a1669
Trey Huffine <treyhuffine@gmail.com>
2015-08-04 15:07:24 -0700

    Update user model with all fields

--- models/user.js
+++ models/user.js
@@ -3,7 +3,22 @@ var userSchema = new mongoose.Schema({
   githubID: String,
   githubData: Object,
   username: String,
-  token: String
+  token: String,
+  devTitle: String,
+  topTech1: String,
+  topTech2: String,
+  topTech3: String,
+  desiredLocation1: String,
+  desiredLocation2: String,
+  desiredLocation3: String,
+  tagline: String,
+  bio: String,
+  personalWebsite: String,
+  email: String,
+  blog: String,
+  stackoverflow: String,
+  twitter: String,
+  linkedin: String
 });
 var User = mongoose.model('User', userSchema);
 
