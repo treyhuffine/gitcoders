562e37e4bac13e7634b5fbce63b2ba655482a6df
Trey Huffine <treyhuffine@gmail.com>
2015-08-03 17:50:19 -0700

    Correctly auth on edit page

--- public/javascripts/components/edit/EditPage.jsx
+++ public/javascripts/components/edit/EditPage.jsx
@@ -1,5 +1,6 @@
 import $ from '../../vendor/jquery.min';
 import React from 'react';
+import CurrentUserStore from '../../stores/CurrentUserStore';
 
 let unauthedEdit = {
   'position': 'absolute',
@@ -8,12 +9,38 @@ let unauthedEdit = {
   'left': '16%'
 }
 
+let getCurrentUserFromStore = () => {
+  return { currentUser: CurrentUserStore.getCurrentUser() };
+}
+
 export default class EditPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = getCurrentUserFromStore();
+    this.onStoreChange = this.onStoreChange.bind(this);
+    console.log("state$$$$$", this.state);
+  }
+  onStoreChange() {
+    this.setState(getCurrentUserFromStore());
+    console.log("change", this.state);
+  }
+  componentDidMount() {
+    CurrentUserStore.addChangeListener(this.onStoreChange);
+  }
+  componentWillUnmount() {
+    CurrentUserStore.removeChangeListener(this.onStoreChange);
+  }
   render() {
-    return (
-      <div style={unauthedEdit}>
-        <h1>Login to edit your account...</h1>
-      </div>
-    )
+    if (!$.isEmptyObject(this.state.currentUser)) {
+      return (
+        <h1>Logged in!</h1>
+      )
+    } else {
+      return (
+        <div style={unauthedEdit}>
+          <h1>Login to edit your account...</h1>
+        </div>
+      )
+    }
   }
 }
