e8116d86c89ed64b0e366d0607a315c3e48ea480
Trey Huffine <treyhuffine@gmail.com>
2015-08-06 19:32:46 -0700

    Add active projects and filter for inactive

--- public/javascripts/components/edit/AllProjectList.jsx
+++ public/javascripts/components/edit/AllProjectList.jsx
@@ -3,8 +3,11 @@ import InactiveProject from './InactiveProject';
 
 export default class AllProjectList extends React.Component {
   render() {
-    let projects = this.props.allRepos.map( (repo, idx) => {
-      return <InactiveProject key={idx} repoData={repo} />
+    let projects = [];
+    this.props.allRepos.forEach( (repo, idx) => {
+      if (!repo.isActive) {
+        projects.push( <InactiveProject key={idx} repoData={repo} /> );
+      }
     })
     console.log("ALL REPOS=+++", this.props);
     return (
--- public/javascripts/components/edit/EditProject.jsx
+++ public/javascripts/components/edit/EditProject.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AllProjectList from './AllProjectList';
+import ActiveProjectList from './ActiveProjectList'
 
 let topBuffer = {
   'marginTop': '30px',
@@ -43,32 +44,7 @@ export default class EditProject extends React.Component {
             </div>
             <div className="row">
               <div className="col m12 s12">
-                <ul className="collection z-depth-1">
-                  <li className="collection-item avatar">
-                    <i className="material-icons circle">folder</i>
-                    <span className="title">Title</span>
-                    <p>First Line <br/>
-                       Second Line
-                    </p>
-                    <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-                  </li>
-                  <li className="collection-item avatar">
-                    <i className="material-icons circle green">insert_chart</i>
-                    <span className="title">Title</span>
-                    <p>First Line <br/>
-                       Second Line
-                    </p>
-                    <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-                  </li>
-                  <li className="collection-item avatar">
-                    <i className="material-icons circle red">play_arrow</i>
-                    <span className="title">Title</span>
-                    <p>First Line <br/>
-                       Second Line
-                    </p>
-                    <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-                  </li>
-                </ul>
+                <ActiveProjectList />
               </div>
             </div>
           </div>
