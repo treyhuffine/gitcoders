ec4b9508f477a348e22de53f470b0cf584cfe6be
Trey Huffine <treyhuffine@gmail.com>
2015-08-09 23:54:29 -0700

    Only show on component mount

--- public/javascripts/components/login/LoginPage.jsx
+++ public/javascripts/components/login/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import LoginCard from './LoginCard';
 
-$('html').css({'background-image': 'url(/images/landing-background.jpg)'})
-$('body').css({'background-image': 'url(/images/landing-background.jpg)'})
 
 export default class LoginPage extends React.Component {
+  componentDidMount() {    
+    $('html').css({'background-image': 'url(/images/landing-background.jpg)'})
+    $('body').css({'background-image': 'url(/images/landing-background.jpg)'})
+  }
   render() {
     return (
       <div className="login-wrapper">
