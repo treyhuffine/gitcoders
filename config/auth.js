577cdd74e3e29a124ba19a193e759426426a3c64
Trey Huffine <treyhuffine@gmail.com>
2015-08-11 11:34:51 -0700

    Update callback URL

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ export default {
   'githubAuth': {
     'clientID': process.env.CLIENT_ID,
     'clientSecret': process.env.CLIENT_SECRET,
-    'callbackURL': 'http://127.0.0.1:3000/auth/callback/github'
+    'callbackURL': 'http://gitcoders.com/auth/callback/github'
   }
 }
